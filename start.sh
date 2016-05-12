70f446e94472909b2a751445600a073558170185
Izzy Galvez <i@igalvez.net>
2016-05-12 16:00:35 +0000

    update folder in crontab rules, update start script to run yoyo script for IzzyBot's avatars.

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "Starting izzybotrun screen."
 screen -d -m -S izzybotrun; sleep 1;
 echo "Sending commands to izzybotrun screen."
 screen -S izzybotrun -X stuff ". ~/.profile
-cd ~/ebooks/
+cd ~/iglvzx_ebooks/
 ebooks start
 "; sleep 1;
 
@@ -19,6 +19,18 @@ echo "Starting izzybotupdate screen."
 screen -d -m -S izzybotupdate; sleep 1;
 echo "Sending commands to izzybotupdate screen."
 screen -S izzybotupdate -X stuff ". ~/.profile
-cd ~/ebooks/
+cd ~/iglvzx_ebooks/
 nodejs update.js
+"; sleep 1;
+
+echo ""
+
+echo "Checking for existing izzybotyoyo screen."
+screen -S izzybotyoyo -X quit || true; sleep 1;
+echo "Starting izzybotyoyo screen."
+screen -d -m -S izzybotyoyo; sleep 1;
+echo "Sending commands to izzybotyoyo screen."
+screen -S izzybotyoyo -X stuff ". ~/.profile
+cd ~/yoyo/
+nodejs bot-yoyo.js
 "; sleep 1;
